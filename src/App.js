3798069da0cc4080521707a768bf93fde9590867
yshwang <hysgold@etevers.com>
2023-07-18 14:17:35 +0900

    버튼 예시뿐만뿐 아니라 textField와 SelectTextField 예시 컴포넌트로 저장

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import BasicButtons from './components/BasicButtons';
+import BasicTextFields from './components/BasicTextFields';
+import SelectTextFields from './components/SelectTextFields';
 
 function App() {
   return (
     <div className="App">
       <BasicButtons/>
+      <BasicTextFields/>
+      <SelectTextFields/>
     </div>
   );
 }
