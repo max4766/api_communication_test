a1bee51b9dcef372ce36d688033988fb69f0d741
yshwang <hysgold@etevers.com>
2023-07-18 18:18:50 +0900

    시험실구분, 조회연도, 조회월 input의 value값이 search를 누르면 alert에 전달됩니다. 그리고 textfield의 onchange 속성과 value 속성, event.target.value, useState를 사용함.

--- src/components/SearchSet.js
+++ src/components/SearchSet.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -15,34 +16,63 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-const ButtonClickEvent = () => {
-  alert('보낼 데이터: ');
-}
-
-
-
 export default function BasicGrid() {
+
+  const [labValue, setLabValue] = useState(labName[0].value);
+  const [yearValue, setYearValue] = useState(selectYear[0].value);
+  const [monthValue, setMonthValue] = useState(selectMonth[0].value);
+
+  // 시험실 구분 SelectTextField의 값이 변경되었을 때 호출되는 핸들러
+  const handleLabChange = (event) => {
+    setLabValue(event.target.value);
+  };
+
+  // 조회연도 SelectTextField의 값이 변경되었을 때 호출되는 핸들러
+  const handleYearChange = (event) => {
+    setYearValue(event.target.value);
+  };
+
+  // 조회월 SelectTextField의 값이 변경되었을 때 호출되는 핸들러
+  const handleMonthChange = (event) => {
+    setMonthValue(event.target.value);
+  };
+
   return (
     <Box sx={{ flexGrow: 1, margin: 4 }}>
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <Item>
-            <SelectTextField label='시험실 구분' defaultValue={labName[0].value} data={labName}/>
+            <SelectTextField 
+              label='시험실 구분' 
+              defaultValue={labName[0].value} 
+              data={labName} 
+              value={labValue} 
+              onChange={handleLabChange}/>
           </Item>
         </Grid>
         <Grid item xs={3}>
           <Item>
-            <SelectTextField label='조회연도' defaultValue={selectYear[0].value} data={selectYear}/>
+            <SelectTextField 
+              label='조회연도' 
+              defaultValue={selectYear[0].value} 
+              data={selectYear} 
+              value={yearValue} 
+              onChange={handleYearChange}/>
           </Item>
         </Grid>
         <Grid item xs={3}>
           <Item>
-            <SelectTextField label='조회월' defaultValue={selectMonth[0].value} data={selectMonth}/>
+            <SelectTextField 
+              label='조회월' 
+              defaultValue={selectMonth[0].value} 
+              data={selectMonth} 
+              value={monthValue} 
+              onChange={handleMonthChange}/>
           </Item>
         </Grid>
         <Grid item xs={2}>
           <Item>
-            <BasicButton onClick={ButtonClickEvent}/>
+            <BasicButton onClick={() => {alert('보낼 데이터: '+labValue+' / '+yearValue+' / '+monthValue);}}/>
           </Item>
         </Grid>
       </Grid>
--- src/components/SelectTextField.js
+++ src/components/SelectTextField.js
@@ -2,19 +2,22 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 
-export default function SelectTextField({label, defaultValue, data}) {
+export default function SelectTextField({label, defaultValue, value, onChange, data}) {
+
   return (
     <TextField
-        id="outlined-select-currency"
-        select
-        fullWidth
-        label={label}
-        defaultValue={defaultValue}
+      id="outlined-select-currency"
+      select
+      fullWidth
+      label={label}
+      defaultValue={defaultValue}
+      value={value}
+      onChange={onChange}
     >
       {data.map((option) => (
-      <MenuItem key={option.value} value={option.value}>
+        <MenuItem key={option.value} value={option.value}>
           {option.label}
-      </MenuItem>
+        </MenuItem>
       ))}
     </TextField>
   );
