4913b4c17a7500db586404af6ac79678b54520de
yshwang <hysgold@etevers.com>
2023-07-18 14:31:24 +0900

    컴포넌트들 다 지움. 필요한거만 골라서 다시하기 위해 세팅

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 
-import BasicButtons from './components/BasicButtons';
-import BasicTextFields from './components/BasicTextFields';
-import SelectTextFields from './components/SelectTextFields';
-
 function App() {
   return (
     <div className="App">
-      <BasicButtons/>
-      <BasicTextFields/>
-      <SelectTextFields/>
+    
     </div>
   );
 }
