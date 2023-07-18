b631856817a5e22754649db9769bc6cb10a49daa
yshwang <hysgold@etevers.com>
2023-07-18 13:37:38 +0900

    API 서버를 통해 클라이언트 서버에 통신을 해보겠습니다.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import BasicButtons from './components/BasicButtons';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicButtons/>
+    </div>
+  );
+}
+
+export default App;
